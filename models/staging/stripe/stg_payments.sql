abbff5d0d04d95ded8ae41e0f1dc5b267e0f53a5
William Trott <wdtrott@gmail.com>
2022-02-24 20:04:38 +0000

    Fix merge issue

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,11 +4,7 @@ with payments as (
     ORDERID as order_id,
     paymentmethod as payment_method,
     status,
-<<<<<<< HEAD
     {{ cents_to_dollars('amount') }} as amount,
-=======
-    amount / 100 as amount,
->>>>>>> f93625137f2a20d4355405f9b5c8ae854d815fbf
     created as created_at
     from {{ source('stripe', 'payment') }}
 )
